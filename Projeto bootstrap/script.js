0e8db1a6d3a6c0b40c2e95a9c0065af51bac3f11
brenobobo <40372411+brenobobo@users.noreply.github.com>
2023-01-18 10:31:07 -0300

    Add files via upload
    Simple project abot control of water of a human body.  O program make de caculator to say how much water o may consume in a day. updating javasctipt and bootstrap

---
+++ Projeto bootstrap/script.js
@@ -0,0 +1,13 @@
+function pegarValoresECalcular(){
+	var altura = document.getElementById("altura").value;
+	var peso = document.getElementById("peso").value;
+
+	var text = altura * peso;
+	
+ 	location.hrf="resultado.html";
+
+
+
+
+
+}
